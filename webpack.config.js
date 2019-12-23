a08710c0f23540521c3d74158aa1164d1ec4eb3e
Edwin18 <igreenbergi78@gmail.com>
2019-12-23 16:35:23 +0200

    6.2 Подключает и настраивает moment.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
-const path = require('path');
+const MomentLocalesPlugin = require(`moment-locales-webpack-plugin`);
+const path = require(`path`);
 
 module.exports = {
-  mode: 'development',
-  entry: './src/main.js',
+  mode: `development`,
+  entry: `./src/main.js`,
   output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'public')
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
   },
-  devtool: 'source-map',
+  devtool: `source-map`,
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    publicPath: 'http://localhost:8080',
+    contentBase: path.join(__dirname, `public`),
+    publicPath: `http://localhost:8080`,
     compress: true,
     watchContentBase: true
   },
@@ -18,8 +19,13 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [`style-loader`, `css-loader`],
       },
     ],
-  }
+  },
+  plugins: [
+    new MomentLocalesPlugin({
+      localesToKeep: [`es-us`],
+    })
+  ]
 };
